b876044101c539bbf5e51d8acf7f4f1054be0a4b
Matthew Mitchener <mattjmitchener@gmail.com>
2014-04-05 16:56:38 -0400

    initializing the package

---
+++ src/Omniphx/Forrest/ForrestServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Omniphx\Forrest;
+
+use Illuminate\Support\ServiceProvider;
+
+class ForrestServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('omniphx/forrest');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
